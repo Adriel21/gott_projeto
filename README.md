6801dd7c079c744dc581c62cf84f2545b65799be
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-11-25 16:32:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gott_projeto
+ 
---
+++ js/script.js
@@ -0,0 +1,13 @@
+const botaoMenu = document.querySelector('.icone'); /*nav h2 a */
+const menu = document.querySelector('.menu'); /* nav ul*/
+
+botaoMenu.addEventListener('click', function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do navegador
+    menu.classList.toggle("menu-aberto");
+
+    if (menu.classList.contains('menu-aberto') ){
+        botaoMenu.innerHTML = "&times;"; 
+    } else {
+        botaoMenu.innerHTML = "&equiv;";
+    }
+});
